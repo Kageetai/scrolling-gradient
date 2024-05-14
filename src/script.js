9a1766aeb6196dbde5fcdfc9eedb28a03038f743
Kageetai <ma@kageetai.net>
2024-05-14 10:54:49 +0200

    adjust CSS class name for clarity

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import debounce from "lodash/debounce";
 
-let className = "scrolled";
+let className = "scrollable";
 let observer = new ResizeObserver(
   debounce((entries) => {
     entries.forEach(() => {
