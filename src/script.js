95d2ef2165165ca12a489c7521e4d6419d7d7693
Kageetai <ma@kageetai.net>
2024-07-03 17:41:24 +0200

    introducing layers, space toggles and animation hack to detect scrolling

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
-import debounce from "lodash/debounce";
-
-let className = "scrollable";
-let observer = new ResizeObserver(
-  debounce((entries) => {
-    entries.forEach(() => {
-      if (document.body.scrollHeight > window.innerHeight * 1.5) {
-        document.body.classList.add(className);
-      } else {
-        document.body.classList.remove(className);
-      }
-    });
-  }, 400),
-);
-observer.observe(document.querySelector("#scrollObserver"));
+// import debounce from "lodash/debounce";
+//
+// let className = "scrollable";
+// let observer = new ResizeObserver(
+//   debounce((entries) => {
+//     entries.forEach(() => {
+//       if (document.body.scrollHeight > window.innerHeight * 1.5) {
+//         document.body.classList.add(className);
+//       } else {
+//         document.body.classList.remove(className);
+//       }
+//     });
+//   }, 400),
+// );
+// observer.observe(document.querySelector("#scrollObserver"));
